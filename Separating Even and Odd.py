756663afd14554a2281290a35f3b6a6859ac226c
CruzAndrei <cruzandrei237@gmail.com>
2023-08-04 11:36:28 +0800

    pseudocode

---
+++ Separating Even and Odd.py
@@ -0,0 +1,7 @@
+#write a python program that reads a text file named numbers.txt that contains 20integers. 
+#The program will create two other text file; the first text file will be named even.txt that will contains all even numbers from the numbers.txt. 
+#The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+#open the three text files
+#read the numbers.txt that contains 20integers in each line    
+#if the numbers are even
+#if the numbers are odd
